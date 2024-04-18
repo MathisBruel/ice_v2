c91ee4ba833e2c83ae2f2088547d45d7f70e87de
thomas-vinet <thomas.vinet@viacesi.fr>
2024-04-18 13:50:37 +0200

    Add typecpl

--- COMMON/LIB-CONTEXT-CENTRAL/include/Data/Cpl.h
+++ COMMON/LIB-CONTEXT-CENTRAL/include/Data/Cpl.h
@@ -16,12 +16,27 @@ class Cpl
 
 public:
 
+    enum CplType {
+        UnknownType = 0,
+        Feature,
+        Trailer,
+        Test,
+        Teaser,
+        Rating,
+        Advertisement,
+        Short,
+        Transitional,
+        PSA,
+        Policy,
+        Live_CPL
+    };
+
     Cpl();
     ~Cpl();
 
     void setId(int id) {this->id = id;}
     void setDatas(std::string uuid, std::string name);
-    void setCplInfos(std::string pathCpl, std::string pathSync, std::string sha1Sync);
+    void setCplInfos(std::string pathCpl, std::string pathSync, std::string sha1Sync, CplType typeCpl);
 
     Query* createQuery();
     Query* updateQuery();
@@ -38,6 +53,7 @@ public:
     std::string getUuid() {return uuid;}
     std::string getName() {return name;}
     std::string getSha1Sync() {return sha1Sync;}
+    CplType gettypeCpl() {return typeCpl;}
     std::string getCplPath() {return pathCpl;}
     std::string getSyncPath() {return pathSync;}
 
@@ -48,6 +64,7 @@ private:
     std::string uuid;
     std::string name;
     std::string sha1Sync;
+    CplType typeCpl;
     // -- specific for synchro
     std::string pathCpl;
     std::string pathSync;
