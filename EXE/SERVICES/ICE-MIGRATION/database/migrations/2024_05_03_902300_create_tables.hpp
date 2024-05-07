1cec0ccf88602287aa577107c5f1f5f1cedf2845
thomas-vinet <thomas.vinet@viacesi.fr>
2024-05-07 16:17:11 +0200

    init WebCentral Database

--- EXE/SERVICES/ICE-MIGRATION/database/migrations/2024_05_03_902300_create_tables.hpp
+++ EXE/SERVICES/ICE-MIGRATION/database/migrations/2024_05_03_902300_create_tables.hpp
@@ -13,19 +13,159 @@ namespace Migrations
         /*! Run the migrations. */
         void up() const override
         {
-            Schema::create("posts", [](Blueprint &table)
+            Schema::create("auditorium", [](Blueprint &table)
             {
-                table.id();
+                table.id().from(4); /* AUTO_INCREMENT */
+                table.integer("id_cinema");
+                table.string("name", 45); /* Varchar(45) */
+                table.integer("room");
+                table.integer("type_ims");
+                table.string("ip_ims", 45); /* Varchar(45) */
+                table.integer("port_ims");
+                table.string("user_ims", 45); /* Varchar(45) */
+                table.string("pass_ims", 45); /* Varchar(45) */
 
-                table.string(NAME);
-                table.timestamps();
+                table.unique("ip_ims");
+            });
+            Schema::create("cinema", [](Blueprint &table)
+            {
+                table.id().from(4); /* AUTO_INCREMENT */
+                table.string("name", 45); /* Varchar(45) */
+
+                table.unique("name");
+            });
+            Schema::create("cpl", [](Blueprint &table)
+            {
+                table.id().from(4); /* AUTO_INCREMENT */
+                table.string("uuid", 45); /* Varchar(45) */
+                table.string("name", 200); /* max 255 */
+                table.integer("type_cpl"); 
+                table.string("sha1_sync", 40); /* Varchar(45) */
+                table.string("path_cpl", 200); /* max 255 */
+                table.string("path_sync", 200).nullable(); /* max 255 */
+
+                table.unique("uuid");
+            });
+            Schema::create("feature", [](Blueprint &table)
+            {
+                table.id().from(4); /* AUTO_INCREMENT */
+                table.string("name", 45); /* Varchar(45) */
+
+                table.unique("name");
+            });
+
+            Schema::create("group", [](Blueprint &table)
+            {
+                table.id().from(4); /* AUTO_INCREMENT */
+                table.string("name", 45); /* Varchar(45) */
+                table.string("description", 200).nullable();
+                
+                table.unique("name");
+            });
+            Schema::create("link_cinema_group", [](Blueprint &table)
+            {
+                table.id().from(4); /* AUTO_INCREMENT */
+                table.integer("id_cinema");
+                table.integer("id_group");
+
+            });
+            Schema::create("link_cinema_release", [](Blueprint &table)
+            {
+                table.id().from(4); /* AUTO_INCREMENT */
+                table.integer("id_release");
+                table.integer("id_cinema");
+
+            });
+            Schema::create("link_cpl_release", [](Blueprint &table)
+            {
+                table.id().from(4); /* AUTO_INCREMENT */
+                table.integer("id_release");
+                table.integer("id_cpl");
+
+            });
+            Schema::create("link_group_release", [](Blueprint &table)
+            {
+                table.id().from(4); /* AUTO_INCREMENT */
+                table.integer("id_release");
+                table.integer("id_group");
+
+            });
+            Schema::create("link_script_release", [](Blueprint &table)
+            {
+                table.id().from(4); /* AUTO_INCREMENT */
+                table.integer("id_release");
+                table.integer("id_script");
+
+            });
+            Schema::create("link_cpl_script", [](Blueprint &table)
+            {
+                table.id().from(4); /* AUTO_INCREMENT */
+                table.integer("id_cpl");
+                table.integer("id_script");
+
+            });
+            Schema::create("release", [](Blueprint &table)
+            {
+                table.id().from(4); /* AUTO_INCREMENT */
+                table.integer("id_feature");
+                table.date("date_in");
+                table.date("date_out").nullable();
+                table.string("name", 45); /* Varchar(45) */
+                table.integer("id_parent_release").nullable();
+                table.string("global_pattern", 45); /* Varchar(45) */
+                table.string("specific_pattern", 45); /* Varchar(45) */
+                table.integer("finalized").defaultValue(0);
+            });
+            Schema::create("release_cut", [](Blueprint &table)
+            {
+                table.id().from(4); /* AUTO_INCREMENT */
+                table.integer("id_release");
+                table.string("decription", 200).nullable(); /* max 255 */
+                table.integer("position");
+                table.integer("size");
+
+            });
+            Schema::create("script", [](Blueprint &table)
+            {
+                table.id().from(4); /* AUTO_INCREMENT */
+                table.string("name", 45); /* Varchar(45) */
+                table.string("path", 200); /* max 255 */
+                table.string("cis_name", 45); /* Varchar(45) */
+                table.string("lvi_name", 45); /* Varchar(45) */
+                table.integer("type");
+                table.string("version", 45); /* Varchar(45) */
+                table.string("sha1Lvi", 40); /* Varchar(45) */
+
+            });
+            Schema::create("server", [](Blueprint &table)
+            {
+                table.id().from(7); /* AUTO_INCREMENT */
+                table.integer("id_auditorium");
+                table.integer("type_server");
+                table.ipAddress("ip"); /* Varchar(45) */
+
+                table.unique("ip");
             });
         }
 
         /*! Reverse the migrations. */
         void down() const override
         {
-            Schema::dropIfExists("posts");
+            Schema::dropIfExists("auditorium");
+            Schema::dropIfExists("cinema");
+            Schema::dropIfExists("cpl");
+            Schema::dropIfExists("feature");
+            Schema::dropIfExists("group");
+            Schema::dropIfExists("link_cinema_group");
+            Schema::dropIfExists("link_cinema_release");
+            Schema::dropIfExists("link_cpl_release");
+            Schema::dropIfExists("link_group_release");
+            Schema::dropIfExists("link_script_release");
+            Schema::dropIfExists("link_cpl_script");
+            Schema::dropIfExists("release");
+            Schema::dropIfExists("release_cut");
+            Schema::dropIfExists("script");
+            Schema::dropIfExists("server");
         }
     };
 
