a0a8bd97bf8667cc181b16a47e255d596541363b
Martin Lagrange <martin.lagrange@cgrcinemas.fr>
2025-06-23 18:55:09 +0200

    Refactoring Content ops - Edit Exception text

--- COMMON/LIB-CONTEXT-CENTRAL/src/ContentOpsBoundary/BoundaryManager.cpp
+++ COMMON/LIB-CONTEXT-CENTRAL/src/ContentOpsBoundary/BoundaryManager.cpp
@@ -5,13 +5,13 @@ std::string BoundaryManager::GetAllContentsAsXml() {
 }
 
 std::string BoundaryManager::GetContentAsXml(int contentId) {
-    throw std::logic_error("GetAllContentsAsXml not implemented");
+    throw std::logic_error("GetContentAsXml not implemented");
 }
 
 void BoundaryManager::UpdateContent(int contentId) {
-    throw std::logic_error("GetAllContentsAsXml not implemented");
+    throw std::logic_error("UpdateContent not implemented");
 }
 
 void BoundaryManager::CreateContent() {
-    throw std::logic_error("GetAllContentsAsXml not implemented");
+    throw std::logic_error("CreateContent not implemented");
 }
